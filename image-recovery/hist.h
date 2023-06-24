5711421a8baa1836aedc573a452f48da48c0842e
jhonataplt <jhonata.demuner@gmail.com>
2023-06-24 14:27:19 -0300

    Folder Refactoring

---
+++ image-recovery/hist.h
@@ -0,0 +1,27 @@
+#ifndef HIST_H
+#define HIST_H
+
+typedef struct hist Hist;
+typedef struct hist_node HistNode;
+
+Hist *create_empty_hist();
+
+char** splitString(char* str, char separator);
+
+void insert_into_the_hist(Hist *h, int *v, char *locality, char *path);
+
+void histIterator(char *inputPath, Hist *h);
+
+void getVector(char* filePath, int* vector, int vectorSize);
+
+void printHist(Hist *h);
+
+double euclideanDistance(int *vector1, int *vector2, int size);
+
+void calculateDistances(int *userImgVector, Hist *h);
+
+void freeHist(Hist *h);
+
+void sortHist(Hist *h);
+
+#endif
